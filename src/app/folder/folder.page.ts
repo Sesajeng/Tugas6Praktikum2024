87cfab218ae2eec228989dee547a3127f6f8f289
Sesajeng <sesaajeng780@gmail.com>
2024-10-29 16:18:51 +0700

    first commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -8,10 +9,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
+  public isInboxPage: boolean = false;
+  public isDisabled = true; // Variabel untuk mengontrol status reorder
   private activatedRoute = inject(ActivatedRoute);
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.isInboxPage = this.folder === 'inbox';
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // Logika untuk mengatur ulang item
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Selesaikan pengurutan
+    ev.detail.complete();
+  }
+
+  toggleReorder() {
+    this.isDisabled = !this.isDisabled; // Mengubah status reorder
   }
 }
